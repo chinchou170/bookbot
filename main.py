be7aad286899eebaf950c35545567b4f133c4812
chinchou170 <gwenmreed@gmail.com>
2025-09-05 23:58:20 -0700

    holy crap

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
-from stats import (
-    get_book_text,
-    count_words,
-    lower_text,
-    count_characters,
-    chars_to_dicts,
-)
+import sys
+from stats import get_book_text, count_words, lower_text, count_characters, chars_to_dicts
 
 def main():
-    path = "books/frankenstein.txt"
+
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    path = sys.argv[1]
     text = get_book_text(path)
     num_words = count_words(text)
     lowered = lower_text(text)
     frequency = count_characters(lowered)
     sorted_char_dicts = chars_to_dicts(frequency)
+
 
     print(f"============ BOOKBOT ============\n Analyzing book found at {path}...\n ----------- Word Count ----------\n Found {num_words} total words\n --------- Character Count -------\n")
     for entry in sorted_char_dicts:
